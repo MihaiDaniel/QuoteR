89892ad76ced8557c3e321e85bfac17b28048475
Daniel Mihai Dumitru <d.dumitru@izysolutions.com>
2023-05-05 12:48:03 +0300

    Refactor FormLifecycleService to have a simpler system with a single timer

--- Quoter/Quoter.App/Services/Forms/FormLifecycleService.cs
+++ Quoter/Quoter.App/Services/Forms/FormLifecycleService.cs
@@ -8,80 +8,71 @@ namespace Quoter.App.Services.Forms
 	/// </summary>
 	public class FormLifecycleService : IFormLifecycleService
 	{
-		class MonitoredForm
-		{
-			public Guid Id { get; set; }
-			public IMonitoredForm Form { get; set; }
-			public System.Timers.Timer Timer { get; set; }
+		private readonly object _lock = new object();
 
-			public MonitoredForm(IMonitoredForm monitoredForm, System.Timers.Timer timer)
-			{
-				Form = monitoredForm;
-				Timer = timer;
-				Id = Guid.NewGuid();
-			}
-		}
-
-		private readonly List<MonitoredForm> _monitoredForms;
 		private readonly ILogger _logger;
+		private readonly Dictionary<IMonitoredForm, DateTime> _dicMonitoredForms;
 
 		public FormLifecycleService(ILogger logger)
 		{
 			_logger = logger;
-			_monitoredForms = new List<MonitoredForm>();
+			_dicMonitoredForms = new Dictionary<IMonitoredForm, DateTime>();
+
+			System.Timers.Timer timer = new System.Timers.Timer(1000);
+			timer.AutoReset = true;
+			timer.Elapsed += (sender, e) => ElapsedTimerEvent();
+			timer.Start();
 		}
 
-		public void CloseDelayed(IMonitoredForm form, int seconds)
+
+		void IFormLifecycleService.CloseDelayed(IMonitoredForm form, int seconds)
 		{
-			_logger.Debug($"Monitoring for {seconds} seconds");
-
-			System.Timers.Timer timerCloseDelay = new(seconds * 1000);
-			timerCloseDelay.AutoReset = true;
-			MonitoredForm monitoredForm = new(form, timerCloseDelay);
-			
-			timerCloseDelay.Elapsed += (sender, e) => ElapsedTimerEventCloseDelayed(monitoredForm);
-			timerCloseDelay.Start();
-
-			_monitoredForms.Add(monitoredForm);
+			_logger.Debug($"Monitoring {form.GetType()} for {seconds} seconds");
+			lock (_lock)
+			{
+				_dicMonitoredForms.Add(form, DateTime.Now.AddSeconds(seconds));
+			}
 		}
 
-		public void EventFormClosing(IMonitoredForm form)
+		void IFormMonitor.EventFormClosing(IMonitoredForm form)
 		{
 			_logger.Debug($"{form.GetType()}");
-
-			MonitoredForm? monitoredForm = _monitoredForms.FirstOrDefault(m => m.Form == form);
-			if (monitoredForm != null)
+			lock (_lock)
 			{
-				if (monitoredForm.Timer.Enabled)
+				
+				_dicMonitoredForms.Remove(form);
+			}
+		}
+
+		private void ElapsedTimerEvent()
+		{
+			lock (_lock)
+			{
+				IEnumerable<IMonitoredForm> lstFormsToClose = _dicMonitoredForms.Where(d => d.Value <= DateTime.Now).Select(d => d.Key);
+				foreach (IMonitoredForm form in lstFormsToClose)
 				{
-					monitoredForm.Timer.Stop();
-					monitoredForm.Timer.Dispose();
-					_monitoredForms.Remove(monitoredForm);
+					TryCloseForm(form);
 				}
 			}
 		}
 
-		private void ElapsedTimerEventCloseDelayed(MonitoredForm monitoredForm)
+		private void TryCloseForm(IMonitoredForm form)
 		{
-			_logger.Debug($"Try to autoclose form");
-
-			EnumFormCloseState state = monitoredForm.Form.IsClosable();
-			if (state == EnumFormCloseState.IsClosable)			// Can close
+			_logger.Debug($"{form.GetType()}");
+			EnumFormCloseState state = form.IsClosable();
+			switch (state)
 			{
-				monitoredForm.Timer.Stop();
-				monitoredForm.Timer.Dispose();
-				_monitoredForms.Remove(monitoredForm);
-				monitoredForm.Form.Close();
-			}
-			else if (state == EnumFormCloseState.NotClosable)	// Retry close every 1 second
-			{
-				monitoredForm.Timer.Interval = 1000; 
-			}
-			else												// Disposed
-			{
-				monitoredForm.Timer.Stop();
-				monitoredForm.Timer.Dispose();
-				_monitoredForms.Remove(monitoredForm);
+				case EnumFormCloseState.IsClosable:
+					_dicMonitoredForms.Remove(form);
+					form.Close();
+					break;
+				case EnumFormCloseState.NotClosable:
+					// Nothing to do, just delay the close
+					_dicMonitoredForms[form].AddSeconds(2);
+					break;
+				case EnumFormCloseState.Disposed:
+					_dicMonitoredForms.Remove(form);
+					break;
 			}
 		}
 	}
--- Quoter/Quoter.App/Services/Forms/IFormLifecycleService.cs
+++ Quoter/Quoter.App/Services/Forms/IFormLifecycleService.cs
@@ -4,11 +4,15 @@
 	/// Interface for handling the lifecycle of forms, it is used to automatically close a form
 	/// after a period of time
 	/// </summary>
+	/// <remarks>
+	/// <see cref="FormLifecycleService"/>
+	/// </remarks>
 	public interface IFormLifecycleService : IFormMonitor
 	{
 		/// <summary>
-		/// Close a <see cref="IMonitoredForm"/> after a number of seconds. Starts a timer
-		/// and tries to close the form if it's closable after the expired period.
+		/// Close a <see cref="IMonitoredForm"/> after a number of seconds.
+		/// Tries to close the form if it's closable after the expired period.
+		/// If form is not closable it delays the close time (by 2 sec) untill it's closable
 		/// </summary>
 		void CloseDelayed(IMonitoredForm form, int seconds);
 	}
