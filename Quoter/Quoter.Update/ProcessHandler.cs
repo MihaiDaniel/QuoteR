3f8ea086224228cfb7e265b1e7228b5ccb552556
Daniel <daniel.dumitru@hn-services.com>
2023-05-10 23:13:31 +0300

    add Quoter.Update

---
+++ Quoter/Quoter.Update/ProcessHandler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quoter.Update
+{
+	public class ProcessHandler
+	{
+		public bool HasWriteRightOnFolder(string folder)
+		{
+			try
+			{
+				string testFile = Path.Combine(folder, "_test");
+				File.WriteAllText(testFile, "1");
+				File.Delete(testFile);
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Logger.Error(ex, "Can't write test file");
+				return false;
+			}
+		}
+
+		public void KillProcessIfRunning(string processName)
+		{
+			if (Process.GetProcessesByName(processName).Length > 0)
+			{
+				Logger.Info("Shutting down process: " + processName);
+				Process[] arrProcess = Process.GetProcessesByName(processName);
+				foreach (Process process in arrProcess)
+				{
+					process.Kill();
+					process.WaitForExit();
+					process.Dispose();
+				}
+			}
+		}
+
+		public void StartProcess(string exePath, string arguments)
+		{
+			try
+			{
+				Logger.Info("Starting process: " + exePath);
+				Process.Start(exePath, arguments);
+			}
+			catch (Win32Exception ex)
+			{
+				Logger.Error(ex, "Error starting process: " + exePath);
+			}
+		}
+
+		public void RestartProcessAsAdmin(string args)
+		{
+			System.Diagnostics.ProcessStartInfo StartInfo = new System.Diagnostics.ProcessStartInfo
+			{
+				UseShellExecute = true, //<- for elevation
+				Verb = "runas",  //<- for elevation
+				WorkingDirectory = Environment.CurrentDirectory,
+				FileName = "Quoter.Update.exe",
+				Arguments = args
+			};
+			System.Diagnostics.Process p = System.Diagnostics.Process.Start(StartInfo);
+		}
+
+
+
+
+
+	}
+}
---
+++ Quoter/Quoter.Update/UpdateHandler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quoter.Update
+{
+	public class UpdateHandler
+	{
+		private BackupHandler _backupHandler;
+		private ProcessHandler _processHandler;
+
+		public UpdateHandler()
+		{
+			_backupHandler = new BackupHandler();
+			_processHandler = new ProcessHandler();
+		}
+
+		public bool TryUpdate(string installFolderPath, string applicationExeName, string updateZipPath)
+		{
+			try
+			{
+				Logger.Info("Begining update.");
+
+				_processHandler.KillProcessIfRunning(Path.GetFileNameWithoutExtension(applicationExeName));
+
+				_backupHandler.BackupCurrentVersion(installFolderPath, applicationExeName);
+				if (!_backupHandler.IsBackupAvailable)
+				{
+					return false; // return if backup failed just to be safe
+				}
+
+				// Get zip and unzip
+				string updateFolderPath = UnZipUpdate(updateZipPath);
+
+				// Copy and replace data
+				ApplyUpdate(installFolderPath, updateFolderPath);
+
+				// Delete update files
+				Directory.Delete(updateFolderPath, true);
+
+				Logger.Info("Update finished successfully.");
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Logger.Error(ex, "Error occured during update.");
+
+				if (_backupHandler.IsBackupAvailable)
+				{
+					_backupHandler.RollbackUpdate();
+				}
+				return false;
+			}
+		}
+
+		private string UnZipUpdate(string zipPath)
+		{
+			string updateName = Path.GetFileNameWithoutExtension(zipPath);
+			string updateFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), updateName);
+
+			ZipFile.ExtractToDirectory(zipPath, updateFolder);
+			return updateFolder;
+		}
+
+		private void ApplyUpdate(string installFolderPath, string updateFolderPath)
+		{
+			string[] arrFiles = Directory.GetFiles(updateFolderPath);
+			foreach (string file in arrFiles)
+			{
+				string appFilePath = Path.Combine(installFolderPath, Path.GetFileName(file));
+				File.Copy(file, appFilePath, true);
+			}
+		}
+	}
+}
