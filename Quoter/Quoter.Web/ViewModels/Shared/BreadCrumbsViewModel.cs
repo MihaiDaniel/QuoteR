cf3d08c38eb77baa2abff25aa12e61731d3700a6
Daniel Mihai Dumitru <dmihaidumitru@gmail.com>
2023-11-19 13:51:52 +0200

    improve the ui of create app versions page, add breadcrumbs support

---
+++ Quoter/Quoter.Web/ViewModels/Shared/BreadCrumbsViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace Quoter.Web.ViewModels.Shared
+{
+	/// <summary>
+	/// ViewModel for displaying breadcrumbs and their names and routes
+	/// </summary>
+	public class BreadCrumbsViewModel
+	{
+		/// <summary>
+		/// The current page name (the last "crumb" in the bread crumbs unclickable)
+		/// </summary>
+		public string CurrentPageName { get; set; }
+
+		/// <summary>
+		/// The routes that the user can go to between Home and Current page
+		/// </summary>
+		public IEnumerable<RouteViewModel> Routes { get; set; }
+
+		public BreadCrumbsViewModel(string currentPageName)
+		{
+			CurrentPageName = currentPageName;
+			Routes = new List<RouteViewModel>();
+		}
+
+		public BreadCrumbsViewModel(string currentPageName, params RouteViewModel[] routes)
+		{
+			CurrentPageName = currentPageName;
+			Routes = routes;
+		}
+	}
+}
---
+++ Quoter/Quoter.Web/ViewModels/Shared/RouteViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Quoter.Web.ViewModels.Shared
+{
+	public class RouteViewModel
+	{
+		public string Name { get; set; }
+
+		public string Route { get; set; }
+
+		public RouteViewModel(string name, string route)
+		{
+			Name = name;
+			Route = route;
+		}
+	}
+}
