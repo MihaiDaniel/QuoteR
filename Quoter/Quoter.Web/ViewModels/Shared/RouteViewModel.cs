a8b320429b47f31fbafd33f8b6e849f1a1d1d666
Daniel Mihai Dumitru <dmihaidumitru@gmail.com>
2024-02-29 00:26:53 +0200

    move partials in other Shared folder

--- Quoter/Quoter.Web/ViewModels/Shared/RouteViewModel.cs
+++ Quoter/Quoter.Web/ViewModels/Shared/RouteViewModel.cs
@@ -1,9 +1,19 @@
 ﻿namespace Quoter.Web.ViewModels.Shared
 {
+	/// <summary>
+	/// View model for a route that the user can access
+	/// </summary>
 	public class RouteViewModel
 	{
+		/// <summary>
+		/// Displayed name of the route
+		/// </summary>
 		public string Name { get; set; }
 
+		/// <summary>
+		/// The route of the page that can be accessed
+		/// Ex: /Dashboard/AppVersions/Index
+		/// </summary>
 		public string Route { get; set; }
 
 		public RouteViewModel(string name, string route)
