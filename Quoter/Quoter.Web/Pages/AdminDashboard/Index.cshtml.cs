6e319d80fe1647c0a162f60e6d6f8736c602017f
Daniel Mihai Dumitru <dmihaidumitru@gmail.com>
2024-02-29 05:08:05 +0200

    basic implementation of the admin dashboard

--- Quoter/Quoter.Web/Pages/AdminDashboard/Index.cshtml.cs
+++ Quoter/Quoter.Web/Pages/AdminDashboard/Index.cshtml.cs
@@ -1,12 +1,72 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Quoter.Shared.Enums;
+using Quoter.Web.Data;
 
 namespace Quoter.Web.Pages.AdminDashboard
 {
-    public class IndexModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	/// <summary>
+	/// Admin Dashboard index page
+	/// </summary>
+	public class IndexModel : PageModel
+	{
+		private readonly ApplicationDbContext _context;
+
+		public IndexViewModel ViewModel { get; set; }
+
+		public IndexModel(ApplicationDbContext context)
+		{
+			_context = context;
+			ViewModel = new IndexViewModel();
+		}
+
+		public async void OnGet()
+		{
+			ViewModel.LatestVersion = await _context.AppVersions
+				.Where(v => v.IsAvailable == true)
+				.OrderByDescending(v => v.CreationDate)
+				.Select(v => v.Version)
+				.FirstOrDefaultAsync() ?? " - ";
+			ViewModel.VersionsNo = await _context.AppVersions.CountAsync();
+			ViewModel.VersionsDownloaded = await _context.AppVersionDownloads
+				.Where(vd => vd.AppVersion!.Type == EnumVersionType.Installer)
+				.CountAsync();
+			ViewModel.VersionsUpdates = await _context.AppVersionDownloads
+				.Where(vd => vd.AppVersion!.Type == EnumVersionType.UpdateZip)
+				.CountAsync();
+
+			ViewModel.RegistrationsNo = await _context.AppRegistrations.CountAsync();
+		}
+	}
+
+	/// <summary>
+	/// View model for dashboard page.
+	/// </summary>
+	public class IndexViewModel
+	{
+		/// <summary>
+		/// Total number of collections irespective of language
+		/// </summary>
+		public int CollectionsNo { get; set; }
+
+		/// <summary>
+		/// The top language and amount of collections downloaded in that language
+		/// </summary>
+		public int TopLanguageDownloads { get; set; }
+
+		/// <summary>
+		/// Total number of downloaded collections
+		/// </summary>
+		public int CollectionsDownloadedNo { get; set; }
+
+		public string LatestVersion { get; set; }
+
+		public int VersionsNo { get; set; }
+
+		public int VersionsDownloaded { get; set; }
+
+		public int VersionsUpdates { get; set; }
+
+		public int RegistrationsNo { get; set; }
+	}
 }
