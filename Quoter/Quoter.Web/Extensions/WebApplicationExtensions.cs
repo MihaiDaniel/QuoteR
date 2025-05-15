6887343189470d9fca9714f7c7156542a105071a
MihaiDaniel <d.dumitru@izysolutions.com>
2025-05-15 17:44:39 +0300

    add default user using environment variables

--- Quoter/Quoter.Web/Extensions/WebApplicationExtensions.cs
+++ Quoter/Quoter.Web/Extensions/WebApplicationExtensions.cs
@@ -1,10 +1,7 @@
-﻿#nullable disable
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using Quoter.Web.Data;
 using Quoter.Web.Data.Entities;
-using Quoter.Web.Models.Configuration;
 
 namespace Quoter.Web.Extensions
 {
@@ -13,31 +10,36 @@ namespace Quoter.Web.Extensions
 		public static void MigrateDatabase(this WebApplication app)
 		{
 			using IServiceScope scope = app.Services.CreateScope();
-			using ApplicationDbContext context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+			using ApplicationDbContext context = scope.ServiceProvider.GetService<ApplicationDbContext>()!;
 			context.Database.Migrate();
 
-			using LogsDbContext logsContext = scope.ServiceProvider.GetService<LogsDbContext>();
+			using LogsDbContext logsContext = scope.ServiceProvider.GetService<LogsDbContext>()!;
 			logsContext.Database.EnsureCreated();
 		}
 
 		public static async Task SeedDatabase(this WebApplication app, ConfigurationManager configuration)
 		{
 			using IServiceScope scope = app.Services.CreateScope();
-			using ApplicationDbContext context = scope.ServiceProvider.GetService<ApplicationDbContext>();
-			using UserManager<ApplicationUser> userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+			using ApplicationDbContext context = scope.ServiceProvider.GetService<ApplicationDbContext>()!;
+			using UserManager<ApplicationUser> userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>()!;
 
-			IOptionsMonitor<UsersConfiguration> optionsMonitor = scope.ServiceProvider.GetService<IOptionsMonitor<UsersConfiguration>>();
-			UsersConfiguration usersConfiguration = optionsMonitor.CurrentValue;
+			string? administrator = Environment.GetEnvironmentVariable("QUOTER_ADMIN_USER");
+			string? password = Environment.GetEnvironmentVariable("QUOTER_ADMIN_PASS");
 
-			ApplicationUser? user = await userManager.FindByEmailAsync(usersConfiguration.DefaultUser.Email);
+			if(string.IsNullOrEmpty(administrator) || string.IsNullOrEmpty(password))
+			{
+				throw new ArgumentException("Environment variables QUOTER_ADMIN_USER and QUOTER_ADMIN_PASS must be set for the application to run!");
+			}
+
+			ApplicationUser user = await userManager.FindByNameAsync(administrator);
 			if(user == null)
 			{
 				ApplicationUser appUser = new()
 				{
-					Email = usersConfiguration.DefaultUser.Email,
-					UserName = usersConfiguration.DefaultUser.UserName,
+					Email = $"{administrator}@gmail.com",
+					UserName = administrator,
 				};
-				await userManager.CreateAsync(appUser, usersConfiguration.DefaultUser.Password);
+				await userManager.CreateAsync(appUser, password);
 			}
 
 		}
